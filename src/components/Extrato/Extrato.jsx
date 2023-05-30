b12b35004e23d10bcfaa5135985b7b81b8238b52
Eduardo Mangeli <eduardo@mangeli.com.br>
2023-05-30 20:52:13 -0300

    data binding

--- src/components/Extrato/Extrato.jsx
+++ src/components/Extrato/Extrato.jsx
@@ -1,25 +1,32 @@
 import ItemExtrato from "../ItemExtrato/ItemExtrato";
 import Container from "./Style";
 import transacoes from "../../data/transacoes.json";
+import { useState, useRef } from "react";
 
+const Extrato = () => {
+    const valor = useRef();
+    const [texto, setTexto] = useState('');
 
-const Extrato = () => (
-    <Container>
-        {
-            transacoes.map(
-                (ele, ind) => (
-                    <ItemExtrato
-                        key={ind}
-                        cat={ele.categoria}
-                        inst={ele.instituicao}
-                        data={ele.data}
-                        descri={ele.descricao}
-                        valor={ele.valor}
-                    />
-                )
-            )
-        }
-    </Container>
-);
+    return (
+            <Container>
+                {
+                    transacoes.map(
+                        (ele, ind) => (
+                            <ItemExtrato
+                                key={ind}
+                                cat={ele.categoria}
+                                inst={ele.instituicao}
+                                data={ele.data}
+                                descri={ele.descricao}
+                                valor={ele.valor}
+                            />
+                        )
+                    )
+                }
+                <input type='text' onChange={(e) => setTexto(e.target.value)}/>
+                <span ref={valor}>{texto}</span>
+            </Container>
+    );
+};
 
 export default Extrato;
